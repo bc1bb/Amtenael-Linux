8a357c442618eee005218bbbbfe58cdca607e6b3
Jus de Patate_ <jusdepatate@protonmail.com>
2020-01-19 16:03:33 +0100

    Mon dieu qu'ai-je fait?

---
+++ Makefile.bat
@@ -0,0 +1,4 @@
+@echo off
+REM AmtenaelLauncher Builder pour Windows
+REM fuck les verifications le support de Windows sera fait a la zeub, aller on compile
+pyinstaller --onefile --noconsole AmtenaelLauncher.py
